bc848839e284cc85e0363e17c05f58198d0c8fcd
Yosep Ryan Agusta <yosep.ryan.yra@gmail.com>
2020-02-25 21:43:28 +0700

    Updates

---
+++ precache-manifest.cbf54fb5ca03c6e084e6136451027423.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598ea735e4c6da2800077a7f37760c6f",
+    "url": "/basketscore/index.html"
+  },
+  {
+    "revision": "a4d0e5ac7d16efc1d639",
+    "url": "/basketscore/static/js/2.7b33a1f7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/basketscore/static/js/2.7b33a1f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d1ce61d7e44dd6f80e",
+    "url": "/basketscore/static/js/main.df94938a.chunk.js"
+  },
+  {
+    "revision": "b560b8b6b188cc501f76",
+    "url": "/basketscore/static/js/runtime-main.568b341d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bba7194742f21119150b395047db0dcc.js"
+  "/basketscore/precache-manifest.cbf54fb5ca03c6e084e6136451027423.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/basketscore/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
